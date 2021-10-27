58f68ef212e13c0d6e9051d83ba2c1646040710b
TUKich <TUK.eSports@yandex.ru>
2021-10-27 05:14:45 +0300

    1j2s

---
+++ lab1j/TestProject1/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ConsoleApp3;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Program p = new Program();
+            bool a = p.To(3, 2, 2);
+            Assert.AreEqual(a, true);
+        }
+
+        [Test]
+        [TestCase(1, 2, 2, false)]
+        [TestCase(3, 2, 2, true)]
+        [TestCase(6, -2, -4, true)]
+        public void Test2(int R, int x, int y, bool a)
+        {
+            Program p = new Program();
+            bool d = p.To(R, x, y);
+            Assert.AreEqual(d,a);
+        }
+    }
+}
---
+++ lab1j/lab1Jija/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        
+        public  bool To(int R, int x, int y)
+        {
+            if(((x >= 0) &&(y >= 0) && (x * x + y * y <= R * R)) || ((x < 0) && (y < 0) && (y >= -x - R))) return true;
+            else return false;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
