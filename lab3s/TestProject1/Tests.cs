da1750da9404e358009b44cee0aa7a1f2765eb6c
TUKich <TUK.eSports@yandex.ru>
2021-11-02 06:43:47 +0300

    Seva3l

---
+++ lab3s/TestProject1/Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using lab3k;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("12 5132534634 1 676789"," 12 5132534634 1 676789")]
+        [TestCase("6969 7878"," 6969 7878")]
+        [TestCase("321 123 213"," 321 123 213")]
+        public void TestFill(String name, String otvet)
+        {
+            Program p = new Program();
+            String o =p.Fill(name);
+            Assert.AreEqual(o,otvet);
+
+        }
+
+        [Test]
+        [TestCase("12 5132534634 1 676789","676789"," 12 5132534634 1")]
+        [TestCase("6969 7878","7878"," 6969")]
+        [TestCase("321 123 213","123"," 321 213")]
+        public void Remove(String name, String remove, String otvet)
+        {
+            Program p = new Program();
+            String o = p.remove(name, remove);
+            Assert.AreEqual(o,otvet);
+        }
+
+        [Test]
+        [TestCase("12 5132534634 1 676789"," 676789 1 5132534634 12")]
+        [TestCase("6969 7878"," 7878 6969")]
+        [TestCase("321 123 213"," 213 123 321")]
+        public void Back(String name, String otvet)
+        {
+            Program p = new Program();
+            String o =p.back(name);
+            Assert.AreEqual(o,otvet);
+        }
+
+        [Test]
+        [TestCase("12 5132534634 1 676789","1", true)]
+        [TestCase("6969 7878","676789",false)]
+        [TestCase("321 123 213","213",true)]
+        public void Contains(String name,String search, Boolean otvet)
+        {
+            Program p = new Program();
+            Boolean o =p.con(name,search);
+            Assert.AreEqual(o,otvet);
+        }
+    }
+}
---
+++ lab3s/lab3k/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab3k
+{
+    public class Program
+    {
+        public class DoublyLinkedList<T> : IEnumerable<T>  
+    {
+        Spisok<T> head; 
+        Spisok<T> tail; 
+        int count;  
+ 
+        
+        public void Add(T data)
+        {
+            Spisok<T> node = new Spisok<T>(data);
+ 
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public void AddFirst(T data)
+        {
+            Spisok<T> node = new Spisok<T>(data);
+            Spisok<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        
+        public bool Remove(T data)
+        {
+            Spisok<T> current = head;
+ 
+            
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if(current!=null)
+            {
+                
+                if(current.Next!=null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    
+                    tail = current.Previous;
+                }
+ 
+                
+                if(current.Previous!=null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+ 
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+ 
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+ 
+        public bool Contains(T data)
+        {
+            Spisok<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+         
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+ 
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Spisok<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+ 
+        public IEnumerable<T> BackEnumerator()
+        {
+            Spisok<T> current = tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+    }
+        public static void Main(String[] args){}
+        
+        public class Spisok<T>
+        {
+            public T Data { get; set; }
+            public Spisok<T> Previous { get; set; }
+            public Spisok<T> Next { get; set; }
+        
+            public Spisok(T data)
+            {
+                Data = data;
+            }
+        }
+
+        public String Fill(String name)
+        {
+            DoublyLinkedList<long> s = new DoublyLinkedList<long>();
+            String[] names = name.Split(' ');
+            String t = null;
+            long[] l = new long[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                l[i] = long.Parse(names[i]);
+            }
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                s.Add(l[i]);
+            }
+
+            t = "";
+            foreach (var y in s)
+            {
+                t =t + " " +y;
+            }
+            return t;
+        }
+
+        public String remove(String name, String remove)
+        {
+            DoublyLinkedList<long> s = new DoublyLinkedList<long>();
+            String[] names = name.Split(' ');
+            String t = null;
+            long[] l = new long[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                l[i] = long.Parse(names[i]);
+            }
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                s.Add(l[i]);
+            }
+
+            long re = long.Parse(remove);
+            s.Remove(re);
+            t = "";
+            foreach (var y in s)
+            {
+                t =t + " " +y;
+            }
+            return t;
+        }
+
+        public String back(String name)
+        {
+            DoublyLinkedList<long> s = new DoublyLinkedList<long>();
+            String[] names = name.Split(' ');
+            long[] l = new long[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                l[i] = long.Parse(names[i]);
+            }
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                s.Add(l[i]);
+            }
+
+            String t = "";
+            foreach (var y in s.BackEnumerator())
+            {
+                t =t + " " +y;
+            }
+            return t;
+        }
+
+        public Boolean con(String name, String search)
+        {
+            DoublyLinkedList<long> s = new DoublyLinkedList<long>();
+            String[] names = name.Split(' ');
+            long[] l = new long[names.Length];
+            for (int i = 0; i < names.Length; i++)
+            {
+                l[i] = long.Parse(names[i]);
+            }
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                s.Add(l[i]);
+            }
+
+            long se = long.Parse(search);
+            return s.Contains(se);
+        }
+
+    }
+    
+
+    
+}
