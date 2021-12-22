61904b3f9c409815b1986c7c46c06eb55fed7391
TUKich <TUK.eSports@yandex.ru>
2021-12-23 01:09:38 +0300

    7

---
+++ lab7/TestProject1/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using lab7;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase("12","5")]
+        [TestCase("1212","32")]
+        public void Test1(string c, string ex)
+        {
+            Program p = new Program();
+            Assert.AreEqual(true, ex.Contains(p.threein16(c)));
+        }
+        [TestCase("-1","null")]
+        [TestCase("a","null")]
+        public void Test2(string c, string ex)
+        {
+            Program p = new Program();
+            Assert.AreEqual(true, ex.Contains(p.threein16(c)));
+        }
+        [TestCase("120","F")]
+        [TestCase("0","0")]
+        public void Test3(string c, string ex)
+        {
+            Program p = new Program();
+            Assert.AreEqual(true, ex.Contains(p.threein16(c)));
+        }
+        [TestCase("210012000221220","ABCDEF")]
+        [TestCase("1011102120201200","FEDCBA")]
+        public void Test4(string c, string ex)
+        {
+            Program p = new Program();
+            Assert.AreEqual(true, ex.Contains(p.threein16(c)));
+        }
+    }
+}
---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Numerics;
+
+namespace lab7
+{
+     public class Program
+    {
+        public string threein16(string c)
+        {
+            
+            try
+            {
+                
+                
+                long u316 = Convert.ToInt64(c);
+                for (int i = 0; i < c.Length; i++)
+                {
+                    if (u316%10 == 0 || u316%10 == 1 || u316%10 == 2)
+                    {
+                        continue;
+                    }
+                    throw new Exception();
+                }
+                string y = "";
+                double temp = 0;
+                double temp3 = 0;
+                double temp2 = 0;
+                for (double i = 0; i < c.Length; i++)
+                {
+                    temp3 = u316 % 10;
+                    u316 = u316 / 10;
+                    temp+=(temp3)*Math.Pow(3,i);
+                }
+
+                long temp1 = Convert.ToInt64(temp);
+                for (; temp1 / 16 >-1;temp1 = temp1 / 16)
+                {
+                    temp2 = temp1 % 16;
+                    
+                    if (temp2 == 10) {y = "A" + y; continue;}
+                    if (temp2 == 11) {y = "B" + y; continue;}
+                    if (temp2 == 12) {y = "C" + y; continue;}
+                    if (temp2 == 13) {y = "D" + y; continue;}
+                    if (temp2 == 14) {y = "E" + y; continue;}
+                    if (temp2 == 15) {y = "F" + y; continue;}
+                    
+                    
+                    if(temp1/16 ==  0 && temp1<15) break;
+                    y = temp2 + y;
+
+                }
+
+                return y;
+            }
+            catch (Exception e)
+            {
+                return "null";
+                throw;
+            }
+            
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            
+            Console.WriteLine(p.threein16("210012000221220"));
+        }
+    }
+}
