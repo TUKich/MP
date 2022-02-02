6d2d51e82d74c94604a73d6a65aff4f8ca92f052
TUKich <TUK.eSports@yandex.ru>
2022-02-03 02:59:56 +0300

    03.02.2022 ЮРА

---
+++ lab2u/TestProject1/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using lab2u;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(1, 2, 3, 4)]
+        [TestCase(23, 43, 54, 21)]
+        public void add(int i1,int i2, int i3, int i4)
+        {
+            Program p = new Program();
+            p.add(i1,i2,i3,i4);
+            Boolean b = p.peek() == 21 || p.peek() == 4;
+            Assert.True(b);
+        }
+        [TestCase(1, 2, 3, 4)]
+        [TestCase(23, 43, 54, 21)]
+        public void Con(int i1,int i2, int i3, int i4)
+        {
+            Program p = new Program();
+            p.add(i1,i2,i3,i4);
+            Boolean b = p.con(4);
+            Assert.AreEqual(b,p.con(4));
+        }
+    }
+}
---
+++ lab2u/lab2u/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace lab2u
+{
+    public class Program
+    {
+         Stack<int> st = new Stack<int>();
+         int[] ints = new int[4];  
+        public void add(int i1,int i2, int i3, int i4)
+        {
+            ints[0] = i1; 
+            ints[1] = i2;
+            ints[2] = i3;
+            ints[3] = i4;
+            foreach (int hh in ints)
+            {
+                st.Push(hh);
+            }
+        }
+        public int peek()
+        {
+            return st.Peek();
+        }
+
+        public Boolean con( int i)
+        {
+            return st.Contains(i);
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
